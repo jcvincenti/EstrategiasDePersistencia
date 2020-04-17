5e1cf41cefe7de3119d90429232fdd0f5d56e146
FabianFrangella <fabianfrangella@gmail.com>
2020-04-17 20:22:28 -0300

    DataService y crearPatogeno test

--- src/test/java/ar/edu/unq/eperdemic/PatogenoServiceTest.kt
+++ src/test/java/ar/edu/unq/eperdemic/PatogenoServiceTest.kt
@@ -3,17 +3,29 @@ package ar.edu.unq.eperdemic
 import ar.edu.unq.eperdemic.modelo.Patogeno
 import ar.edu.unq.eperdemic.persistencia.dao.jdbc.JDBCPatogenoDAO
 import ar.edu.unq.eperdemic.services.impl.PatogenoServiceImpl
+import ar.edu.unq.eperdemic.utils.jdbc.DataServiceJDBC
+import org.junit.After
 import org.junit.Assert
+import org.junit.Before
 import org.junit.jupiter.api.Test
 
 
 class PatogenoServiceTest {
     val patogenoService = PatogenoServiceImpl(JDBCPatogenoDAO())
+    val dataService = DataServiceJDBC()
 
+    @Before
+    fun init() {
+        dataService.crearSetDeDatosIniciales()
+    }
     @Test
     fun testCrearPatogeno(){
         var patogeno = Patogeno("coronavirus")
-        patogenoService.crearPatogeno(patogeno)
-        TODO("cuando este pusheado el get de patogeno, se hace el assert del crear")
+        Assert.assertEquals(1, patogenoService.crearPatogeno(patogeno));
+    }
+
+    @After
+    fun cleanUp() {
+        dataService.eliminarTodo()
     }
 }
--- src/test/java/ar/edu/unq/eperdemic/utils/jdbc/DataServiceJDBC.kt
+++ src/test/java/ar/edu/unq/eperdemic/utils/jdbc/DataServiceJDBC.kt
@@ -1,13 +1,31 @@
 package ar.edu.unq.eperdemic.utils.jdbc
 
+import ar.edu.unq.eperdemic.modelo.Patogeno
+import ar.edu.unq.eperdemic.persistencia.dao.jdbc.JDBCConnector
+import ar.edu.unq.eperdemic.persistencia.dao.jdbc.JDBCPatogenoDAO
 import ar.edu.unq.eperdemic.utils.DataService
+import java.sql.Connection
 
 class DataServiceJDBC : DataService {
+    val dao = JDBCPatogenoDAO()
+
     override fun crearSetDeDatosIniciales() {
-        TODO("not implemented")
+        var tiposDePatogenos = mutableListOf("bacteria", "hongo", "protozo", "virus")
+        tiposDePatogenos.forEach{
+            patogeno -> dao.crear(Patogeno(patogeno))
+        }
     }
 
     override fun eliminarTodo() {
-        TODO("not implemented")
+        TODO("hacer un eliminar solo de los datos que insertamos en el test, acá estoy limpiando toda la tabla")
+        return JDBCConnector.execute { conn: Connection ->
+            val ps = conn.prepareStatement("DELETE FROM patogeno")
+            ps.executeUpdate()
+            if (ps.updateCount != 1) {
+                throw RuntimeException("No se pudo eliminar los datos de la tabla patogeno")
+            }
+            ps.close()
+            null
+        }
     }
 }
