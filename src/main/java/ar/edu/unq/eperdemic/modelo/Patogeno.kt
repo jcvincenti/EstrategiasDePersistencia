df9af3e2004184429eaa267fdef1cc0b592ee752
FabianFrangella <fabianfrangella@gmail.com>
2020-04-23 13:33:01 -0300

    test-crear fixeado

--- src/main/java/ar/edu/unq/eperdemic/modelo/Patogeno.kt
+++ src/main/java/ar/edu/unq/eperdemic/modelo/Patogeno.kt
@@ -7,12 +7,29 @@ class Patogeno(val tipo: String) : Serializable{
     var id : Int? = null
     var cantidadDeEspecies: Int = 0
 
-    override fun toString(): String {
-        return tipo
-    }
-
     fun crearEspecie(nombreEspecie: String, paisDeOrigen: String) : Especie{
         cantidadDeEspecies++
         return Especie(this, nombreEspecie, paisDeOrigen)
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Patogeno
+
+        if (tipo != other.tipo) return false
+        if (id != other.id) return false
+        if (cantidadDeEspecies != other.cantidadDeEspecies) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = tipo.hashCode()
+        result = 31 * result + (id ?: 0)
+        result = 31 * result + cantidadDeEspecies
+        return result
+    }
+
 }
