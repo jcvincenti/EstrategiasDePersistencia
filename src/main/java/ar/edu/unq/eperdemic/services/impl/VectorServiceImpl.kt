6a56cb42d5c688c9256233d7a71e8b32b1c8da1d
fabianfrangella <fabianfrangella@gmail.com>
2020-04-25 22:29:50 -0300

    agregada operacion crearVector en service y dao

---
+++ src/main/java/ar/edu/unq/eperdemic/services/impl/VectorServiceImpl.kt
@@ -0,0 +1,33 @@
+package ar.edu.unq.eperdemic.services.impl
+
+import ar.edu.unq.eperdemic.modelo.Especie
+import ar.edu.unq.eperdemic.modelo.Vector
+import ar.edu.unq.eperdemic.persistencia.dao.VectorDAO
+import ar.edu.unq.eperdemic.services.VectorService
+
+class VectorServiceImpl(val vectorDAO: VectorDAO) : VectorService {
+    override fun contagiar(vectorInfectado: Vector, vectores: List<Vector>) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun infectar(vector: Vector, especie: Especie) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun enfermedades(vectorId: Int): List<Especie> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun crearVector(vector: Vector): Vector {
+        vectorDAO.guardar(vector)
+        return vector
+    }
+
+    override fun recuperarVector(vectorId: Int): Vector {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun borrarVector(vectorId: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
