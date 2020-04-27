d7d7f39d6fe66844b47ddcc31eb61aa0998eb965
FabianFrangella <fabianfrangella@gmail.com>
2020-04-27 20:36:25 -0300

    fix a modelo

--- src/main/java/ar/edu/unq/eperdemic/dto/VectorFrontendDTO.kt
+++ src/main/java/ar/edu/unq/eperdemic/dto/VectorFrontendDTO.kt
@@ -14,11 +14,11 @@ class VectorFrontendDTO(val tipoDeVector : TipoDeVector,
         var vector = Vector()
         vector.nombreDeLocacionActual = nombreDeUbicacionPresente
         vector.tipo = tipoDeVector.toString()
-        vector.contagioStrategy = buildContagioStrategy(tipoDeVector)
+        vector.contagioStrategy = buildContagioStrategy()
         return vector
     }
 
-    private fun buildContagioStrategy(tipo: TipoDeVector) : ContagioStrategy{
+    private fun buildContagioStrategy() : ContagioStrategy{
         return when(tipoDeVector) {
             TipoDeVector.Persona -> Persona()
             TipoDeVector.Animal -> Animal()
