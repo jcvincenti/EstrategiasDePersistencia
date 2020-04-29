37f3bdae5be6fa78a5d7f30984c749ee13247499
Juan Cruz Vincenti <juancruzvincenti@gmail.com>
2020-04-29 19:45:01 -0300

    Movido el recuperar mediante string a hibernateDao

--- src/main/java/ar/edu/unq/eperdemic/persistencia/dao/hibernate/HibernateUbicacionDAO.kt
+++ src/main/java/ar/edu/unq/eperdemic/persistencia/dao/hibernate/HibernateUbicacionDAO.kt
@@ -5,8 +5,4 @@ import ar.edu.unq.eperdemic.persistencia.dao.UbicacionDAO
 import ar.edu.unq.eperdemic.services.runner.TransactionRunner
 
 open class HibernateUbicacionDAO: HibernateDAO<Ubicacion>(Ubicacion::class.java), UbicacionDAO {
-    override fun recuperar(nombreUbicacion: String): Ubicacion {
-        val session = TransactionRunner.currentSession
-        return session.get(Ubicacion::class.java, nombreUbicacion)
-    }
 }
