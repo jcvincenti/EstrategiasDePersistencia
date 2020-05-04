58da24e0f7fb44e48e1ce3d1e4117e95eba939f6
fabianfrangella <fabianfrangella@gmail.com>
2020-05-04 01:22:56 -0300

    eliminados imports sin usar

--- src/main/java/ar/edu/unq/eperdemic/services/impl/UbicacionServiceImpl.kt
+++ src/main/java/ar/edu/unq/eperdemic/services/impl/UbicacionServiceImpl.kt
@@ -8,7 +8,6 @@ import ar.edu.unq.eperdemic.services.UbicacionService
 import ar.edu.unq.eperdemic.services.exceptions.EntityNotFoundException
 import ar.edu.unq.eperdemic.services.runner.TransactionRunner
 import ar.edu.unq.eperdemic.services.utils.ObjectStructureUtils
-import org.omg.CORBA.Object
 
 class UbicacionServiceImpl(val ubicacionDAO: UbicacionDAO) : UbicacionService {
 
