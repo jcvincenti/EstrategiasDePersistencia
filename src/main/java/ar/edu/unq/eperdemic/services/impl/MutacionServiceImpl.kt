7690de695169639c5082071902e08cad7ba04113
fabianfrangella <fabianfrangella@gmail.com>
2020-05-13 20:25:17 -0300

    mutar en mutacionService

--- src/main/java/ar/edu/unq/eperdemic/services/impl/MutacionServiceImpl.kt
+++ src/main/java/ar/edu/unq/eperdemic/services/impl/MutacionServiceImpl.kt
@@ -2,16 +2,23 @@ package ar.edu.unq.eperdemic.services.impl
 
 import ar.edu.unq.eperdemic.modelo.Mutacion
 import ar.edu.unq.eperdemic.persistencia.dao.MutacionDAO
+import ar.edu.unq.eperdemic.persistencia.dao.hibernate.HibernatePatogenoDAO
 import ar.edu.unq.eperdemic.services.MutacionService
 import ar.edu.unq.eperdemic.services.exceptions.EntityNotFoundException
 import ar.edu.unq.eperdemic.services.runner.TransactionRunner
 import ar.edu.unq.eperdemic.services.utils.ObjectStructureUtils
 
 class MutacionServiceImpl(val mutacionDAO: MutacionDAO) : MutacionService {
-
-
+    val patogenoService = PatogenoServiceImpl(HibernatePatogenoDAO())
     override fun mutar(especieId: Int, mutacionId: Int) {
-        TODO("Not yet implemented")
+        var especie = TransactionRunner.runTrx {
+            patogenoService.recuperarEspecie(especieId)
+        }
+        TransactionRunner.runTrx {
+            val mutacion = mutacionDAO.recuperar(mutacionId)?: throw EntityNotFoundException("La entidad Mutacion con id ${mutacionId} no existe")
+            especie!!.mutar(mutacion!!)
+            patogenoService.actualizarEspecie(especie)
+        }
     }
 
     override fun crearMutacion(mutacion: Mutacion): Mutacion {
