becbac71a3decb3fda5d52168ed8e5aab4b0f6d4
fabianfrangella <fabianfrangella@gmail.com>
2020-05-16 20:08:34 -0300

    cambiado a refresh el cascade type

--- src/main/java/ar/edu/unq/eperdemic/modelo/Especie.kt
+++ src/main/java/ar/edu/unq/eperdemic/modelo/Especie.kt
@@ -11,7 +11,7 @@ class Especie() {
     lateinit var nombre: String
     @Column(name = "pais_de_origen")
     lateinit var paisDeOrigen: String
-    @ManyToOne(cascade = [CascadeType.MERGE])
+    @ManyToOne(cascade = [CascadeType.REFRESH])
     lateinit var patogeno: Patogeno
     @ManyToMany(mappedBy = "especies", fetch = FetchType.LAZY)
     var vectores: MutableSet<Vector> = HashSet()
