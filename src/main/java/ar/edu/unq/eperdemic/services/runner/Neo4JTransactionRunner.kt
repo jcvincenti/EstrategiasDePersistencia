d8248aa838bd845694bc0327c02019eb38c11ebc
fabianfrangella <fabianfrangella@gmail.com>
2020-06-05 16:10:52 -0300

    eliminado atributo innecesario

--- src/main/java/ar/edu/unq/eperdemic/services/runner/Neo4JTransactionRunner.kt
+++ src/main/java/ar/edu/unq/eperdemic/services/runner/Neo4JTransactionRunner.kt
@@ -3,8 +3,6 @@ package ar.edu.unq.eperdemic.services.runner
 import org.neo4j.driver.Transaction
 
 object Neo4JTransactionRunner {
-    var currentTrx: Transaction? = null
-
     fun <T> runTrx(bloque: (Transaction)->T): T {
         return Neo4JSessionFactoryProvider.instance.createSession().writeTransaction {
             //codigo de negocio
