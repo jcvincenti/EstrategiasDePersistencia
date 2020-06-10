dae6d8af09b5629ac8817b76d52776f239fe803d
andeehr <andeehr@gmail.com>
2020-06-09 22:31:14 -0300

    Fixeado el UbicacionMuyLejana

--- src/main/java/ar/edu/unq/eperdemic/persistencia/dao/neo4j/Neo4JUbicacionDAO.kt
+++ src/main/java/ar/edu/unq/eperdemic/persistencia/dao/neo4j/Neo4JUbicacionDAO.kt
@@ -38,15 +38,17 @@ class Neo4JUbicacionDAO : INeo4JUbicacionDAO{
         }
     }
 
-    //TODO: Refactorizar por ubicaciones lindantes
     override fun esUbicacionMuyLejana(origen: String, destino: String) : Boolean {
         val tx = Neo4JTransaction.transaction
-        val query = "MATCH (n:Ubicacion {nombreUbicacion: \$origen})-[r*]->(m:Ubicacion {nombreUbicacion: \$destino})" +
-                " RETURN SIGN(COUNT(r)) = 0"
-        val result = tx!!.run(query, Values.parameters(
+        val query = "MATCH (n:Ubicacion {nombreUbicacion: \$origen}), (m:Ubicacion {nombreUbicacion: \$destino})" +
+                " RETURN not exists ((n)-->(m)) "
+        val exc = tx!!.run(query, Values.parameters(
                 "origen", origen,
                 "destino", destino))
-        return result.single().values()[0].asBoolean()
+        val result = exc.list { record: Record ->
+            record[0].asBoolean()
+        }
+        return result[0]
     }
 
     override fun capacidadDeExpansion(nombreDeUbicacion: String, movimientos: Int, tiposDeCamino: List<String>): Int {
