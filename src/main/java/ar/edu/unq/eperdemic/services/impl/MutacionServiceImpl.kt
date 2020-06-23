f6a935ddc13d656e6e08071e62e96c502cd9515a
fabianfrangella <fabianfrangella@gmail.com>
2020-06-22 22:41:34 -0300

    agregados logeos de mutacion

--- src/main/java/ar/edu/unq/eperdemic/services/impl/MutacionServiceImpl.kt
+++ src/main/java/ar/edu/unq/eperdemic/services/impl/MutacionServiceImpl.kt
@@ -1,18 +1,24 @@
 package ar.edu.unq.eperdemic.services.impl
 
+import ar.edu.unq.eperdemic.modelo.Evento
 import ar.edu.unq.eperdemic.modelo.Mutacion
 import ar.edu.unq.eperdemic.persistencia.dao.MutacionDAO
 import ar.edu.unq.eperdemic.persistencia.dao.hibernate.HibernatePatogenoDAO
+import ar.edu.unq.eperdemic.persistencia.dao.mongo.MongoEventoDAO
 import ar.edu.unq.eperdemic.services.MutacionService
 import ar.edu.unq.eperdemic.services.utils.ObjectStructureUtils
 import ar.edu.unq.eperdemic.services.utils.validateEntityExists
 
 class MutacionServiceImpl(val mutacionDAO: MutacionDAO) : MutacionService {
+    val eventoDao = MongoEventoDAO()
+
     val patogenoService = PatogenoServiceImpl(HibernatePatogenoDAO())
     override fun mutar(especieId: Int, mutacionId: Int) {
         val especie = patogenoService.recuperarEspecie(especieId)
         especie.mutar(recuperarMutacion(mutacionId))
         patogenoService.actualizarEspecie(especie)
+        eventoDao.logearEvento(Evento(especie.patogeno.tipo, especie.nombre
+                , "Se muto la especie ${especie.nombre} "))
     }
 
     override fun crearMutacion(mutacion: Mutacion): Mutacion {
--- src/main/java/ar/edu/unq/eperdemic/services/impl/PatogenoServiceImpl.kt
+++ src/main/java/ar/edu/unq/eperdemic/services/impl/PatogenoServiceImpl.kt
@@ -1,10 +1,12 @@
 package ar.edu.unq.eperdemic.services.impl
 
 import ar.edu.unq.eperdemic.modelo.Especie
+import ar.edu.unq.eperdemic.modelo.Evento
 import ar.edu.unq.eperdemic.modelo.Patogeno
 import ar.edu.unq.eperdemic.persistencia.dao.EspecieDAO
 import ar.edu.unq.eperdemic.persistencia.dao.PatogenoDAO
 import ar.edu.unq.eperdemic.persistencia.dao.hibernate.HibernateEspecieDAO
+import ar.edu.unq.eperdemic.persistencia.dao.mongo.MongoEventoDAO
 import ar.edu.unq.eperdemic.services.PatogenoService
 import ar.edu.unq.eperdemic.services.exceptions.*
 import ar.edu.unq.eperdemic.services.utils.ObjectStructureUtils
@@ -15,6 +17,7 @@ import ar.edu.unq.eperdemic.services.utils.validateEntityExists
 class PatogenoServiceImpl(val patogenoDAO: PatogenoDAO) : PatogenoService {
 
     val especieDAO: EspecieDAO = HibernateEspecieDAO()
+    val eventoDao = MongoEventoDAO()
 
     override fun crearPatogeno(patogeno: Patogeno): Int {
         ObjectStructureUtils.checkEmptyAttributes(patogeno)
@@ -40,12 +43,15 @@ class PatogenoServiceImpl(val patogenoDAO: PatogenoDAO) : PatogenoService {
 
     override fun agregarEspecie(id: Int, nombreEspecie: String, paisDeOrigen: String): Especie {
         var especieCreada: Especie? = null
+        var patogeno: Patogeno? = null
         TransactionRunner.runTrx {
             validateEntityExists<Patogeno>(id)
-            val patogeno = patogenoDAO.recuperar(id)
+            patogeno = patogenoDAO.recuperar(id)
             especieCreada = patogeno!!.crearEspecie(nombreEspecie,paisDeOrigen)
             especieDAO.guardar(especieCreada!!)
         }
+        eventoDao.logearEvento(Evento(patogeno!!.tipo, especieCreada!!.nombre
+                , "Se creo la especie ${especieCreada!!.nombre}"))
         return especieCreada!!
     }
 
