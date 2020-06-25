3c92238b99e6d3b9472281eb2814160daa68dad4
Fabian Frangella <fabianfrangella@gmail.com>
2020-06-25 20:15:21 -0300

    indentacion

--- src/main/java/ar/edu/unq/eperdemic/modelo/Evento.kt
+++ src/main/java/ar/edu/unq/eperdemic/modelo/Evento.kt
@@ -15,14 +15,15 @@ class Evento() {
     var especies: List<String>? = null
     var explicacion: String? = null
 
-
     companion object {
         fun buildEventoArribo(vector: Vector, explicacion: String) : Evento {
             return Evento(vector.id, vector.nombreDeLocacionActual, explicacion)
         }
+
         fun buildEventoMutacion(especie: Especie, explicacion: String) : Evento {
             return Evento(especie.patogeno.tipo, especie.nombre, explicacion)
         }
+
         fun buildEventoContagio(vectorId: Int?, nombreUbicacion: String?, tipoDePatogeno: String?,
                                 especie: String?, especies: List<String>?, explicacion: String) : Evento {
             return Evento(vectorId, nombreUbicacion, tipoDePatogeno, especie, especies, explicacion)
@@ -54,11 +55,12 @@ class Evento() {
         this.explicacion = explicacion
     }
 
-    private constructor(vector: Vector, vectorInfectado: Vector, explicacion: String) {
+    private constructor(vector: Vector, vectorInfectado: Vector, explicacion: String) : this() {
         this.vectorId = vectorInfectado.id
         this.nombreUbicacion = vector.nombreDeLocacionActual
         this.explicacion = explicacion
     }
+
     private constructor(vectorId: Int?, nombreUbicacion: String?, explicacion: String) : this() {
         this.vectorId = vectorId
         this.nombreUbicacion = nombreUbicacion
